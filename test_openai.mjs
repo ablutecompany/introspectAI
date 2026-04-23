819f67848ca6365d97b3e89f142d47e6dc34d74a
ablute-software <carladias@ablute.pt>
2026-04-23 18:21:47 +0100

    chore: remove test file

--- test_openai.mjs
+++
@@ -1,4 +0,0 @@
-import OpenAI from 'openai';
-const openai = new OpenAI({ apiKey: 'fake-key' });
-console.log('Keys on openai:', Object.keys(openai));
-console.log('typeof openai.chat:', typeof openai.chat);
